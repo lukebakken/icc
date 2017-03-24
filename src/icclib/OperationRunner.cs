72c2123d65021c0e43b3131c28bbdba977996aca
Luke Bakken <luke@bakken.io>
2017-03-24 09:14:48 -0700

    Add some expected output and failing test for OperationRunner

---
+++ src/icclib/OperationRunner.cs
@@ -0,0 +1,26 @@
+﻿namespace icclib
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class OperationRunner
+    {
+        private readonly IEnumerable<int> set;
+
+        public OperationRunner(IEnumerable<int> set)
+        {
+            if (set == null)
+            {
+                throw new ArgumentNullException("set");
+            }
+
+            this.set = set;
+        }
+
+        public IEnumerable<string> Run()
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
---
+++ test/OperationRunnerTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Test
+{
+    using System.Collections.Generic;
+    using icclib;
+    using Xunit;
+
+    public class OperationRunnerTests
+    {
+        [Theory]
+        [InlineData("10 = ((2 + 7) + (0 + 1))")]
+        [InlineData("10 = ((2 + 7) - (0 - 1))")]
+        [InlineData("10 = ((2 + 7) + (1 - 0))")]
+        public void OutputsExpectedExpression(string expr)
+        {
+            var ary = new int[] { 2, 0, 1, 7 };
+            var r = new OperationRunner(ary);
+            IEnumerable<string> exprs = r.Run();
+            Assert.Contains(expr, exprs);
+        }
+    }
+}
